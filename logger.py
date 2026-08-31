0ffcbd948d320478ffc94d60ddda3fe5b1b99321
aswiro <areff.hirad@gmail.com>
2026-09-01 02:25:59 +0500

    feat: add safe application logging

---
+++ logger.py
@@ -0,0 +1,22 @@
+import sys
+
+from loguru import logger
+
+logger.remove()
+logger.add(
+    sys.stdout,
+    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+    level="INFO",
+    colorize=True,
+)
+logger.add(
+    "logs/bot.log",
+    rotation="00:00",
+    retention="10 days",
+    compression="zip",
+    format="{time:YYYY-MM-DD HH:mm:ss} | {level: <8} | {name}:{function}:{line} - {message}",
+    level="DEBUG",
+    enqueue=True,
+)
+
+__all__ = ["logger"]
